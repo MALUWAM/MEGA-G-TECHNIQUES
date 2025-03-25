a399fdd5abd854eb16af7e090d8f85388b002690
MALUWAM <mushe@midimedia.co.za>
2025-03-25 16:21:14 +0200

    Updating

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css"; // Import CSS for styling
 import Blog from "../components/Blog";
@@ -6,33 +6,32 @@ import Blog from "../components/Blog";
 import solar from "../assets/PRESENTATION/WhatsApp Image 2024-12-20 at 13.59.27.jpeg";
 import printing from "../assets/PRESENTATION/WhatsApp Image 2025-03-03 at 20.27.11 (1).jpeg";
 import graphics from "../assets/graphics.jpg";
+import main from "../assets/PRESENTATION/WhatsApp Image 2025-03-24 at 12.49.27_4930bad5.jpg";
 
-
-const images = [
-  solar,
-  graphics,
-  printing,
-];
+const images = [solar, graphics, printing];
 
 const Home = () => {
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("visible");
-        } else {
-          // Remove the 'visible' class when the element is out of view
-          entry.target.classList.remove("visible");
-        }
-      });
-    }, { threshold: 0.5 }); // Trigger when 50% of the element is in view
-  
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+          } else {
+            // Remove the 'visible' class when the element is out of view
+            entry.target.classList.remove("visible");
+          }
+        });
+      },
+      { threshold: 0.5 }
+    ); // Trigger when 50% of the element is in view
+
     // Select all elements with the scroll-animate class
     const elements = document.querySelectorAll(".scroll-animate");
     elements.forEach((element) => {
       observer.observe(element); // Keep observing even after visibility
     });
-  
+
     // Clean up the observer on component unmount
     return () => {
       elements.forEach((element) => {
@@ -52,11 +51,9 @@ const Home = () => {
 
   return (
     <div className="home">
-
-
       <section>
         <div className="container">
-        <h1 className="title">MEGA G TECHNIQUES</h1>
+          <h1 className="title">MEGA G TECHNIQUES</h1>
           <div className="services">
             <div className="service-list scroll-animate">
               <ul>
@@ -84,32 +81,36 @@ const Home = () => {
           </div>
         </div>
       </section>
-      {/* <section class="image-section">
-        <img src="" alt="Description of Image" />
-    </section> */}
-    <section className="introduction">
-      <div className="intro-text">
-        <p>
-          Mega G Techniques is a cutting-edge 3D printing and Renewable energy niche
-          company dedicated to transforming ideas into tangible products. Our mission
-          is simply to deliver innovative, high-quality, and customizable 3D printable
-          solutions integrated with electronics and heavy current products (Renewable
-          energy) to produce extraordinary functional solutions.
-        </p>
-        <p>
-          We do multi-skills service and product delivery such as Electricity installations
-          (House wiring) and repairs, 3D graphics design, and Printing. We house skills such
-          as electrical and Mechanical Engineering, 3D graphics design, 3D printing, and
-          Renewable energy technology.
-        </p>
-      </div>
-      <div className="intro-slider">
-        <img src={images[currentImage]} alt="Showcase" className="intro-img" />
-
-      </div>
-    </section>
-      <Blog/>
-
+      <section className="image-section">
+        <img src={main} alt="Description" />
+      </section>
+      <section className="introduction">
+        <div className="intro-text">
+          <p>
+            Mega G Techniques is a cutting-edge 3D printing and Renewable energy
+            niche company dedicated to transforming ideas into tangible
+            products. Our mission is simply to deliver innovative, high-quality,
+            and customizable 3D printable solutions integrated with electronics
+            and heavy current products (Renewable energy) to produce
+            extraordinary functional solutions.
+          </p>
+          <p>
+            We do multi-skills service and product delivery such as Electricity
+            installations (House wiring) and repairs, 3D graphics design, and
+            Printing. We house skills such as electrical and Mechanical
+            Engineering, 3D graphics design, 3D printing, and Renewable energy
+            technology.
+          </p>
+        </div>
+        <div className="intro-slider">
+          <img
+            src={images[currentImage]}
+            alt="Showcase"
+            className="intro-img"
+          />
+        </div>
+      </section>
+      <Blog />
 
       {/* About Section Preview */}
       <section className="preview scroll-animate">
